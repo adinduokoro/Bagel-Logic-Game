34c2353c0f2d91dbf4ad2407bfeb3dd9f42abba5
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2022-06-15 12:44:51 -0400

    Bagel
    This was an old Bagel game that I created before my Github profile was created.

---
+++ Bagel.py
@@ -0,0 +1,95 @@
+# MODS
+import random
+
+# GLOBAL VARIABLE
+secretNum = ""
+guess = ""
+clue = []
+GUESS_NUM = 1
+game_running = True
+
+# Global VARIABLE SETTINGS
+MAX_GUESSES = 10
+NUM_DIGITS = 3
+
+
+# FUNCTIONS
+# Gets the secret number
+def getSecretNum():
+    global secretNum
+
+    number = list("0123456789")
+    random.shuffle(number)
+    for j in range(NUM_DIGITS):
+        secretNum += number[j]
+
+
+# Gets a valid guess from the user
+def getGuess():
+    global guess, GUESS_NUM
+
+    while len(guess) != NUM_DIGITS:
+        guess = input(f"Guess #{GUESS_NUM}:\n")
+
+
+# this function helps reset the clue after every loop
+def resetClue():
+    global clue
+
+    clue = []
+
+
+# MAIN
+print('''---------------------------------------------------------------------
+Bagels, a deductive logic game.
+
+I am thinking of a 3-digit number with no repeated digits.
+Try to guess what it is. Here are some clues:
+When I say: That means:
+    Pico    One digit is correct but in the wrong position.
+    Fermi   One digit is correct and in the right position.
+    Bagels  No digit is correct.
+
+For example, if the secret number was 248 and your guess was 843, the
+clues would be Fermi Pico.
+---------------------------------------------------------------------''')
+while game_running:
+    print("I have thought up a number.")
+    print(f"You have {MAX_GUESSES} guesses to get it right.")
+    getSecretNum()
+    # print(secretNum)
+    while GUESS_NUM < (MAX_GUESSES + 1):
+        getGuess()
+        if guess == secretNum:
+            print("You got it correct!")
+            print(f"The secret number was [{secretNum}]")
+            break
+        elif guess != secretNum:
+            for i in range(NUM_DIGITS):
+                if guess[i] == secretNum[i]:  # check for matching spot
+                    clue.append("Fermi")
+                elif guess[i] in secretNum:  # check to see if number is in secretNum
+                    clue.append("Pico")
+            if len(clue) != 0:  # Sort the clues in order to not give away the answer
+                clue.sort()
+                clue = " ".join(clue)
+                print(clue)
+            if len(clue) == 0:
+                print("Bagel")
+        # RESET
+        resetClue()
+        guess = ""
+        GUESS_NUM += 1
+    # Ask the user if they want to continue playing the game
+    print(f">>>You ran out of guesses. The secret number was [{secretNum}].")
+    response = input(">>>Do you want to play again? (YES/NO): ").upper()
+    if response == "YES":
+        print()
+        # MASTER RESET
+        guess = ""
+        secretNum = ""
+        GUESS_NUM = 1
+        game_running = True
+    else:
+        game_running = False
+print("exiting...")
